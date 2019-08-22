d8225b486c92cd5468ff895a74da75de6d217f0a
Daniel Clas <danielclas@outlook.es>
2019-08-22 17:17:41 -0300

    Ej. 13 Hecho

--- Ejercicios/Guia Ejercicios/Ejercicio 13/Program.cs
+++ Ejercicios/Guia Ejercicios/Ejercicio 13/Program.cs
@@ -14,7 +14,8 @@ namespace Ejercicio_13
             string DecimalBinario(double). Convierte un número de entero a binario.
             double BinarioDecimal(string). Convierte un número binario a entero.*/
 
-            Console.Title = "Ejercicio 13";            char rta = '1';            string aux = " ";            double num = 0;            do {
+            Console.Title = "Ejercicio 13";            char rta = '1';            string aux = "";            string bin = "";            double num = 0;            do
+            {
 
                 Console.WriteLine("1. Decimal a binario");
                 Console.WriteLine("2. Binario a decimal");
@@ -22,11 +23,20 @@ namespace Ejercicio_13
                 Console.WriteLine("\nOpcion: ");
                 rta = char.Parse(Console.ReadLine());
 
-                if (rta == '2') {
+                if (rta == '2')
+                {
                     Console.WriteLine("Ingrese nro. binario: ");
                     aux = Console.ReadLine();
-                    num=Conversor.BinarioDecimal(aux);
+                    num = Conversor.BinarioDecimal(aux);
                     Console.WriteLine($"Decimal: {num}");
+                }
+                else if (rta == '1')
+                {
+                    Console.WriteLine("Ingrese nro. decimal: ");
+                    num = double.Parse(Console.ReadLine());
+                    bin = Conversor.DecimalBinario(num);
+                    Console.WriteLine($"Binario: {bin}");
+
                 }
 
                 Console.ReadKey();
@@ -41,11 +51,31 @@ namespace Ejercicio_13
     {
         public static string DecimalBinario(double num)
         {
-            string bin=" ";
+            string bin = "";
+            string auxBin = "";
+            StringBuilder aux = new StringBuilder("");
+            int module;
+            int result = (int)num;
 
+            do
+            {
+                module = result % 2;
+                result = result / 2;
+                aux.Append(module.ToString());
 
+            } while (result /2!=1);
+            
+            module = result % 2;
+            result = result / 2;
+            aux.Append(module.ToString());
+            aux.Append(result.ToString());
 
+            auxBin = aux.ToString();
 
+            for (int i = auxBin.Length - 1; i >= 0; i--)
+            {
+                bin += auxBin[i];
+            }
 
             return bin;
 
@@ -53,7 +83,7 @@ namespace Ejercicio_13
 
         public static double BinarioDecimal(string num)
         {
-            double dec=0;
+            double dec = 0;
 
             for (int i = num.Length - 1, exp = 0; i >= 0; i--, exp++)
             {
