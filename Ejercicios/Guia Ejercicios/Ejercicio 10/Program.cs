a70b41282b6d3ea9d0bdc27de852a0caedb3c236
Daniel Clas <danielclas@outlook.es>
2019-08-21 22:04:51 -0300

    Practica C#

--- Ejercicios/Guia Ejercicios/Ejercicio 10/Program.cs
+++ Ejercicios/Guia Ejercicios/Ejercicio 10/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +10,33 @@ namespace Ejercicio_10
   {
     static void Main(string[] args)
     {
+      /*Partiendo de la base del ejercicio anterior, se pide realizar un programa que imprima por pantalla
+      una pirámide como la siguiente:
+                            *
+                           ***
+                          *****
+                         *******
+                        *********
+      Nota: Utilizar estructuras repetitivas y selectivas.*/
+
+      Console.Title = "Ejercicio 10";
+
+      int index = 0;
+      int menos = 1;
+      string aux = "";
+      string pir = "";
+
+      Console.WriteLine("Ingrese un nro.: ");
+      aux = Console.ReadLine();
+
+      if (int.TryParse(aux, out index) && index > 0)
+      {
+        
+
+
+      }
+      
+      Console.ReadKey();
     }
   }
 }
