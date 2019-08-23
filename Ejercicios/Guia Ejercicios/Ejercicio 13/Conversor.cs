d58565817ad41a399d11989ea25bd50d45355104
Daniel Clas <danielclas@outlook.es>
2019-08-23 13:37:40 -0300

    Formato de nro. binario con coma corregido

---
+++ Ejercicios/Guia Ejercicios/Ejercicio 13/Conversor.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_13
+{
+    class Conversor
+    {
+        public static string DecimalBinario(double num)
+        {
+            string bin = "";
+            string auxBin = "";
+            StringBuilder aux = new StringBuilder("");
+            int module;
+            int result = (int)num;
+            int divInt;
+            int times = 0;
+            double fract = num % 1;
+
+            do
+            {
+                module = result % 2;
+                result = result / 2;
+                aux.Append(module.ToString());
+
+            } while (result / 2 != 1);
+
+            module = result % 2;
+            result = result / 2;
+            aux.Append(module.ToString());
+            aux.Append(result.ToString());
+            auxBin = aux.ToString();
+
+            for (int i = auxBin.Length - 1; i >= 0; i--)
+            {
+                bin += auxBin[i];
+            }
+
+            if (fract != 0)
+            {
+                bin += '.';
+                //La parte fraccionaria por dos, guardo el entero
+                do
+                {
+                    fract = fract * 2;
+                    divInt = (int)fract;
+                    if (fract >= 1)
+                    {
+                        fract = fract % 1;
+                    }
+                    bin += divInt.ToString();
+                    times++;
+                } while (times < 4);
+            }
+
+            return bin;
+
+        }
+
+        public static double BinarioDecimal(string num)
+        {
+            double dec = 0;
+
+            for (int i = num.Length - 1, exp = 0; i >= 0; i--, exp++)
+            {
+                if (num[i] == '1')
+                {
+                    dec += Math.Pow(2, exp);
+                }
+            }
+
+            return dec;
+
+        }
+
+        public static string FormatearBinario(string bin)
+        {
+            string newBin = "";
+            int resto = 0;
+            int i;
+            int len = 0;
+
+            if (bin.Contains('.')) {
+                len = bin.IndexOf('.');
+            }
+            else
+            {
+                len = bin.Length;
+            }
+
+            if (len >= 5)
+            {
+                resto = len % 4;
+
+                for (i = 0; i < resto; i++)
+                {
+                    newBin += bin[i];
+                }
+
+                newBin += ' ';
+                for (int j = 1; i < len; i++, j++)
+                {
+                    newBin += bin[i];
+                    if (j % 4 == 0)
+                    {
+                        if (i != len - 1)
+                        {
+                            newBin += ' ';
+                        }
+
+                        j = 0;
+                    }
+                }
+
+                if (bin.Contains('.'))
+                {
+                    //Agrego substring del nro. desde el punto en adelante
+                    newBin += ' ';
+                    newBin += bin.Substring(len);
+                }
+
+            }
+            else
+            {
+                newBin = bin;
+            }
+
+            return newBin;
+
+        }
+    }
+}
--- Ejercicios/Guia Ejercicios/Ejercicio 13/Program.cs
+++ Ejercicios/Guia Ejercicios/Ejercicio 13/Program.cs
@@ -31,7 +31,7 @@ namespace Ejercicio_13
 
                 if (rta == '2')
                 {
-                    Console.WriteLine("Ingrese nro. binario: ");
+                    Console.WriteLine("Ingrese nro. binario (solo entero): ");
                     aux = Console.ReadLine();
                     num = Conversor.BinarioDecimal(aux);
                     aux = Conversor.FormatearBinario(aux);
@@ -41,7 +41,7 @@ namespace Ejercicio_13
                 }
                 else if (rta == '1')
                 {
-                    Console.WriteLine("Ingrese nro. decimal: ");
+                    Console.WriteLine("Ingrese nro. decimal (entero o con coma): ");
                     num = double.Parse(Console.ReadLine());
                     bin = Conversor.DecimalBinario(num);
                     bin = Conversor.FormatearBinario(bin);
@@ -54,117 +54,6 @@ namespace Ejercicio_13
                 Console.Clear();
 
             } while (rta != '3');
-        }
-    }
-
-    class Conversor
-    {
-        public static string DecimalBinario(double num)
-        {
-            string bin = "";
-            string auxBin = "";
-            StringBuilder aux = new StringBuilder("");
-            int module;
-            int result = (int)num;
-            int divInt;
-            int times = 0;
-            double fract = num%1;
-
-            do
-            {
-                module = result % 2;
-                result = result / 2;
-                aux.Append(module.ToString());
-
-            } while (result / 2 != 1);
-
-            module = result % 2;
-            result = result / 2;
-            aux.Append(module.ToString());
-            aux.Append(result.ToString());
-            auxBin = aux.ToString();
-
-            for (int i = auxBin.Length - 1; i >= 0; i--)
-            {
-                bin += auxBin[i];
-            }
-
-            if (fract!=0)
-            {
-                bin += '.';
-                //La parte fraccionaria por dos, guardo el entero
-                do
-                {
-                    fract = fract * 2;
-                    divInt = (int)fract;
-                    if (fract>=1)
-                    {
-                        fract = fract % 1;
-                    }
-                    bin += divInt.ToString();
-                    times++;
-                } while (times<4);
-            }          
-
-            return bin;
-
-        }
-
-        public static double BinarioDecimal(string num)
-        {
-            double dec = 0;
-
-            for (int i = num.Length - 1, exp = 0; i >= 0; i--, exp++)
-            {
-                if (num[i] == '1')
-                {
-                    dec += Math.Pow(2, exp);
-                }
-            }
-
-            return dec;
-
-        }
-
-        public static string FormatearBinario(string bin)
-        {
-            string newBin = "";
-            int resto = 0;
-            int i;
-
-            if (bin.Length >= 5)
-            {
-                resto = bin.Length % 4;
-
-                for (i = 0; i < resto; i++)
-                {
-                    newBin += bin[i];
-                }
-
-                newBin += ' ';
-                for (int j = 1; i < bin.Length; i++, j++)
-                {
-                    newBin += bin[i];
-                    if (j % 4 == 0)
-                    {
-                        if (i != bin.Length - 1)
-                        {
-                            newBin += ' ';
-                        }
-
-                        j = 0;
-                    }
-                }
-
-
-            }
-            else
-            {
-                newBin = bin;
-            }
-
-            return newBin;
-
         }
     }
 }
