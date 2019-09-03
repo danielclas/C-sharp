642d6f070b8f937d02dc6e8060ef8e4423b0fa16
Daniel Clas <danielclas@outlook.es>
2019-09-03 14:07:47 -0300

    Ej. 27 terminado, 28 empezado

--- Ejercicios/Guia Ejercicios/Ejercicio 27/Program.cs
+++ Ejercicios/Guia Ejercicios/Ejercicio 27/Program.cs
@@ -11,7 +11,7 @@ namespace Ejercicio_27
         static void Main(string[] args)
         {
             Console.Title = "Ejercicio 27";
-            Console.WriteLine("Lista:");
+            Console.WriteLine("Lista: \n");
 
             List<int> lista = new List<int>();
             List<int> men = new List<int>();
@@ -81,7 +81,7 @@ namespace Ejercicio_27
                 sCola += ($"{enteros.ElementAt(i)}, ");
             }
 
-            Console.WriteLine("Cola:");
+            Console.WriteLine("Cola: \n");
             Console.WriteLine("Sin ordenar: \n");
             sCola = sCola.Remove(sCola.Length - 2);
             Console.WriteLine(sCola);
@@ -130,6 +130,66 @@ namespace Ejercicio_27
 
             //----------------------------------------------------//
 
+            Console.WriteLine("Pila: ");
+            Stack<int> pila = new Stack<int>();
+            Stack<int> pMen = new Stack<int>();
+            Stack<int> pMay = new Stack<int>();
+            string sPila = "";
+
+            menores = "";
+            mayores = "";
+
+            for (int i = 0; i < 20; i++)
+            {
+                pila.Push(random.Next(-100, 100));
+            }            
+
+            foreach (int n in pila)
+            {
+                if (n<0)
+                {
+                    pMen.Push(n);
+                }
+                else
+                {
+                    pMay.Push(n);
+                }
+
+                sPila += ($"{n}, ");
+            }
+
+            Console.WriteLine("\nPila sin ordenar: \n");
+            sPila = sPila.Remove(sPila.Length - 2);
+            Console.WriteLine(sPila);
+
+            men = pMen.ToList();
+            men.Sort();
+            men.Reverse();
+            pMen = new Stack<int>(men);
+
+            foreach (int n in pMen)
+            {
+                menores += ($"{n}, ");
+            }
+
+            Console.WriteLine("\nMenores ordenados: \n");
+            menores = menores.Remove(menores.Length - 2);
+            Console.WriteLine(menores);
+
+            may = pMay.ToList();
+            may.Sort();
+            pMay = new Stack<int>(may);
+
+            foreach (int n in pMay)
+            {
+                mayores += ($"{n}, ");
+            }
+
+            Console.WriteLine("\nMayores ordenados: \n");
+            mayores = mayores.Remove(mayores.Length - 2);
+            Console.WriteLine(mayores);
+
+            Console.ReadKey();
         }
     }
 }
---
+++ Ejercicios/Guia Ejercicios/Ejercicio 28/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio 28";
+
+            //Utilizar Diccionarios (Dictionary<string, int>)para realizar un contador de palabras, recorriendo el
+            //texto palabra por palabra se deberá lograr que si se trata de una nueva palabra, se la agregará al
+            //diccionario e inicializar su contador en 1, caso contrario se deberá incrementar dicho contador.
+            //Ordenar los resultados de forma descendente por cantidad de apariciones de cada palabra.
+            //Informar mediante un MessageBox el TOP 3 de palabras con más apariciones.
+
+            Dictionary<string, int> dic = new Dictionary<string, int>();
+
+            
+        }
+    }
+
+    public static class Texto
+    {
+        public static string texto = "";
+        private static string[] array = new string[1];
+
+        public static void CapturarTexto(string texto)
+        {
+            Texto.texto = texto;
+            array = Texto.texto.Split(' ');
+        }
+
+
+    }
+}
