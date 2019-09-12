1bb8b1c54da850446997fb9748098c42ff5b0de8
danielclas <danielclas@outlook.es>
2019-09-11 22:05:59 -0300

    Ej. 33

---
+++ Ejercicios/Guia Ejercicios/Ejercicio 33/Program.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_33
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Title = "Ejercicio 33";
+
+      //El indexador leerá la página pedida, siempre y cuando el subíndice se encuentre dentro de un rango
+      //correcto, sino retornará un string vacio “”. En el set, la asignará si esta ya existe. Si el índice es superior al
+      //máximo existente, agregará una nueva página.
+      //Generar el código en el Main para probar la clase.
+
+
+
+
+      Console.ReadKey();
+    }
+  }
+}
