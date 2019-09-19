02dc7f91ee6f093f99bf47b81e53b05601dd2edf
danielclas <danielclas@outlook.es>
2019-09-19 17:00:40 -0300

    Script

--- Ejercicios/Guia Ejercicios/Ejercicio 37/Centralita.cs
+++ Ejercicios/Guia Ejercicios/Ejercicio 37/Centralita.cs
@@ -13,8 +13,18 @@ namespace Ejercicio_37
 
         public float GananciasPorLocal { get; }
         public float GananciasPorProvincial { get;}
-        public float GananciasPorTotal { get;}
+        public float GananciasPorTotal { get { return CalcularGanancia(); } }
         public List<Llamada> Llamadas { get;}
+
+        //l.CalcularGanancia será privado.Este método recibe un Enumerado TipoLlamada y retornará
+        //el valor de lo recaudado, según el criterio elegido(ganancias por las llamadas del tipo Local,
+        //Provincial o de Todas según corresponda).
+        //m.El método Mostrar expondrá la razón social, la ganancia total, ganancia por llamados locales
+        //y provinciales y el detalle de las llamadas realizadas.
+        //n.La lista sólo se inicializará en el constructor por defecto Centralita().
+        //o.Las propiedades GananciaPorTotal, GananciaPorLocal y GananciaPorProvincial retornarán el
+        //precio de lo facturado según el criterio.Dichos valores se calcularán en el método
+        //CalcularGanancia().
 
         public Centralita()
         {
