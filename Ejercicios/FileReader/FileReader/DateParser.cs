5205d2be9a0356560d18855cd87db37412db1699
danielclas <danielclas@outlook.es>
2019-10-17 15:56:16 -0300

    Clipboard reader

---
+++ Ejercicios/FileReader/FileReader/DateParser.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileReader
+{
+    public static class DateParser
+    {
+        static DateTime today8AM;
+        static DateTime yesterday8AM;
+
+        public static DateTime Today8AM { get { return DateParser.today8AM; } }
+        public static DateTime Yesterday8AM { get { return DateParser.yesterday8AM; } }
+
+        static DateParser()
+        {
+            DateParser.today8AM = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 8, 0, 0);
+
+            if (DateTime.Now.DayOfWeek == DayOfWeek.Monday)
+            {
+                //Substracts 3 days so that "yesterday" represents last friday
+                DateParser.yesterday8AM = today8AM.Subtract(TimeSpan.FromDays(3));
+
+            }
+            else
+            {
+                DateParser.yesterday8AM = today8AM.Subtract(TimeSpan.FromDays(1));
+            }
+        }
+
+        public static DateTime DateFromString(string line)
+        {
+            int year, month, day, hour, minute, seconds, fraction;
+
+            //Original format: 2019-10-15 13:27:23.160
+            //Character count: 23
+            //Each substring corresponds to a part of the date
+            year = int.Parse(line.Substring(0, 4));
+            month = int.Parse(line.Substring(5, 2));
+            day = int.Parse(line.Substring(8, 2));
+
+            hour = int.Parse(line.Substring(11, 2));
+            minute = int.Parse(line.Substring(14, 2));
+            seconds = int.Parse(line.Substring(17, 2));
+            fraction = int.Parse(line.Substring(20, 3));
+
+            return new DateTime(year, month, day, hour, minute, seconds, fraction);
+        }
+
+        public static bool ContainedInTimeframe(DateTime date)
+        {
+            return (date >= DateParser.yesterday8AM && date <= DateParser.today8AM);
+        }
+
+        public static string FormatString(DateTime date)
+        {
+            //Format: 10/15/19 8:19 PM
+            //Character count: 19
+            //Used for AM or PM
+            string time = String.Format("{0:H:mm t}m", date);
+            time = time.ToUpper();
+
+            string spaces = time.Length == 7 ? "  " : " ";
+
+            return String.Format("{0:M/d/y} {1}{2}\t", date, time, spaces);
+        }
+    }
+}
--- Ejercicios/FileReader/FileReader/Program.cs
+++ Ejercicios/FileReader/FileReader/Program.cs
@@ -13,32 +13,17 @@ namespace FileReader
         [STAThread]
         static void Main(string[] args)
         {
-<<<<<<< HEAD
             //Original format: 2019-10-15 13:27:23.160
-=======
-            //Format: 2019-10-15 13:27:23.160
->>>>>>> 516764f0ca087541be668b9fdb5baea9724e7e73
             //Character count: 23
 
             string[] clipboardLines = Clipboard.GetText().Split('\n');
-            
+
             DateTime date;
-            DateTime today8AM = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 8, 0, 0);
-            DateTime yesterday8AM;
+            DateTime today8AM=DateParser.Today8AM;
+            DateTime yesterday8AM=DateParser.Yesterday8AM;           
 
-            if (DateTime.Now.DayOfWeek==DayOfWeek.Monday)
-            {
-                //Substracts 3 days so that "yesterday" represents last friday
-                yesterday8AM = today8AM.Subtract(TimeSpan.FromDays(3));
-
-            }
-            else
-            {
-                yesterday8AM = today8AM.Subtract(TimeSpan.FromDays(1));
-            }
-
-            int year, month, day, hour, minute, seconds, fraction, length;
-            string time, fileName, spaces, dateString;
+            int length, from;
+            string fileName, dateString;
             List<string> newLines = new List<string>();
             List<string> dates = new List<string>();
 
@@ -47,49 +32,21 @@ namespace FileReader
                 if (line.Length > 22)
                 {
                     //Corresponds to the date segment of the original string
-                    newLines.Add(line.Substring(6, 23));
+                    //From is used to control for the length of the BotLogID
+                    from = line.IndexOf('\t')+1;
+                    newLines.Add(line.Substring(from, 23));
                 }
             }
 
             foreach (string newLine in newLines)
             {
-                //Each substring corresponds to a part of the date
-                //Params: Substring(startIndex, length)
-                year = int.Parse(newLine.Substring(0, 4));
-                month = int.Parse(newLine.Substring(5, 2));
-                day = int.Parse(newLine.Substring(8, 2));
-
-                hour = int.Parse(newLine.Substring(11, 2));
-                minute = int.Parse(newLine.Substring(14, 2));
-                seconds = int.Parse(newLine.Substring(17, 2));
-                fraction = int.Parse(newLine.Substring(20, 3));
-
-                date = new DateTime(year, month, day, hour, minute, seconds, fraction);
+                date = DateParser.DateFromString(newLine);
                 date = date.Subtract(TimeSpan.FromHours(5));
 
-                //After the 5 hours have been substracted, checks whether it's in the desired timeframe
-                if (date>= yesterday8AM && date<=today8AM)
-                {
-                    //Used for AM or PM
-                    time = String.Format("{0:H:mm t}m", date);
-                    time = time.ToUpper();
-
-                    spaces = time.Length == 7 ? "  " : " ";
-
-<<<<<<< HEAD
-                    dateString = String.Format("{0:M/d/y} {1}{2}\t", date, time, spaces);
-                }
-                else
-                {
-                    dateString = String.Empty;
-                }
-                
+                //After the 5 hours have been substracted, checks whether it's in the desired timeframe              
+                dateString = DateParser.ContainedInTimeframe(date) ? DateParser.FormatString(date): String.Empty;
                 dates.Add(dateString);
-=======
-                dates.Add(String.Format("{0:M/d/y} {1}{2}  ", date, time, spaces));
-                //Format: 10/15/19 8:19 PM
->>>>>>> 516764f0ca087541be668b9fdb5baea9724e7e73
-                //Character count: 19
+                
             }
 
             //newLines is reused to write to clipboard
@@ -101,10 +58,11 @@ namespace FileReader
                 //Each clipboardLine corresponds to a date in the same index
                 if (i < dates.Count && !(String.IsNullOrEmpty(dates[i])))
                 {
-                    //La nueva fecha es insertada, y la original removida
+                    //New date is inserted, and the original removed
                     StringBuilder str = new StringBuilder(clipboardLines[i]);
-                    str.Insert(6, dates[i]);
-                    str.Remove(6 + length, 24);
+                    from = clipboardLines[i].IndexOf('\t')+1;                    
+                    str.Insert(from, dates[i]);
+                    str.Remove(from + length, 24);
                     newLines.Add(str.ToString());
                 }
             }
@@ -113,23 +71,11 @@ namespace FileReader
             fileName = ($"C:\\Users\\Public\\Documents\\{fileName}.xlsx");
             File.Copy("C:\\Users\\Public\\Documents\\template.xlsx", fileName);
 
-<<<<<<< HEAD
             StringBuilder clipboard = new StringBuilder();
 
             foreach (string line in newLines)
             {
-                clipboard.Append(line);               
-=======
-            if (lines.Length==newLines.Count && newLines.Count==dates.Count)
-            {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("\nArchivo leido y creado correctamente");
-            }
-            else
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("\nEl archivo no pudo ser leido correctamente");
->>>>>>> 516764f0ca087541be668b9fdb5baea9724e7e73
+                clipboard.Append(line);
             }
 
             Clipboard.SetText(clipboard.ToString());
