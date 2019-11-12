007180a23caf978b62ffc5632227f1b888e86639
danielclas <danielclas@outlook.es>
2019-11-12 20:04:41 -0300

    ...

---
+++ Ejercicios/Guia Ejercicios/NrosRomanos/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NrosRomanos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("5 es: "+ConvertirARomano(5));
+            Console.ReadKey();
+        }
+
+        public static string ConvertirARomano(int num)
+        {
+            int[] valores = new int[] { 1, 5, 10, 50, 100, 500, 1000 };
+            string[] letras = new string[] { "I", "V", "X", "L", "C", "D", "M" };
+
+            int resto=num;
+            StringBuilder romano = new StringBuilder();
+            string rtnVal = "";
+
+            do
+            {
+                for (var i = 6; i >= 0; i--)
+                {
+                    Console.WriteLine(valores[i]);
+                    if (num - valores[i] * 3 >= 0)
+                    {
+
+                        romano.Append(letras[i]);
+                        resto = num - valores[i] * 3;
+
+                    }
+                    else if (num - valores[i] * 2 >= 0)
+                    {
+
+                        romano.Append(letras[i]);
+                        resto = num - valores[i] * 2;
+
+                    }
+                    else if (num - valores[i] * 1 >= 0)
+                    {
+
+                        romano.Append(letras[i]);
+                        resto = num - valores[i] * 1;
+                    }
+
+                    if (resto==0)
+                    {
+                        break;
+                    }
+                }
+            } while (resto > 0);
+
+            rtnVal = (string)romano.ToString().Reverse();
+
+            return rtnVal;
+        }
+    }
+}
