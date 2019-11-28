40ac7be35ee5e5faf11fc7e71d6227fbbc019f9a
danielclas <danielclas@outlook.es>
2019-11-28 17:28:21 -0300

    Selenium test

---
+++ Ejercicios/Test IWebDriver/Test IWebDriver/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.IO;
+using System.Windows;
+using System.Windows.Forms;
+
+
+namespace Test_IWebDriver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            IWebDriver _driver;
+            _driver = new ChromeDriver();
+            _driver.Navigate().GoToUrl(Clipboard.GetText());
+            string pageSource = _driver.PageSource;
+            File.WriteAllLines(desktop+"\\html.txt", new string []{ pageSource });
+            _driver.Close();
+        }
+    }
+}
